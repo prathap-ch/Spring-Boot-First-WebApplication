f1f6f3ba1b246c2df7eb58530606d788946d5294
pratap <pratap@hp-PC>
2021-07-28 11:18:41 +0530

    First Commit

---
+++ Spring-Boot-First-WebApplication/src/test/java/com/skillUp/SpringBootFirstWebApplication/SpringBootFirstWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillUp.SpringBootFirstWebApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFirstWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
